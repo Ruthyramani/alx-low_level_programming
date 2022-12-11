3a0c8d7cc97b68c723307ad2f51a8a628e744a74
Ruth Charles <cruth7186@gmail.com>
2022-12-11 07:53:21 -0800

    my first C code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main  - using the put function to print a line of code
+ *
+ * Return: 0
+ * /
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+	
