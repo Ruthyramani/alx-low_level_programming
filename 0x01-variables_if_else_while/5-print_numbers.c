f6f4648469a7af31a49d5391a37e94310d6a498b
Ruth Charles <cruth7186@gmail.com>
2022-12-13 15:15:56 -0800

    printed all single digits of numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		print("%i", digit);
+		++digit;
+
+	}
+	print("\n");
+	return (0);
