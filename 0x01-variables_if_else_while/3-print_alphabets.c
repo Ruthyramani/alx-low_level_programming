a73b33c3374d6f111694b89f9b78599f90b1843a
Ruth Charles <cruth7186@gmail.com>
2022-12-13 09:12:21 -0800

    printed alphabets in lower and upper cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - Entry
+ *
+ *Return: 0
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A'
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		++ch;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		++CH;
+	}
+	putchar('\n');
+	return (0);
