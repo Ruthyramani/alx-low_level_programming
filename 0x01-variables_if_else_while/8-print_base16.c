9063ee13294b8c3eb7a731a643d996c24b2e1110
Ruth Charles <cruth7186@gmail.com>
2022-12-13 16:06:03 -0800

    printed numbers to base of lower case

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int digit = 49;
+
+	while (digit <= 102)
+	{
+		putchar(digits);
+
+		if (digit == 57)
+			digit += 39;
+			++digit;
+	}
+	putchar('\n');
+	return (0);
+}
