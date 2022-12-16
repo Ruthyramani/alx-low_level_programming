73920607115f162ee2fe83e8c9e3e71ec2a042bf
Ruth Charles <cruth7186@gmail.com>
2022-12-15 17:17:35 -0800

    printed most numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 /**
- *print_numbers - print from 0 to 9
+ *print_most_numbers - print from 0 to 9
  *Return: void
 */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	char c;
 
