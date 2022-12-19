8ac99d54854c535351a1bfbc8236a2323be9d071
Ruth Charles <cruth7186@gmail.com>
2022-12-19 09:25:41 -0800

    updated a value that points to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - update the value of its point
+ * @n: int to update
+ * Reurn: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is directory that contains the task for pointer,arrrays and strings
