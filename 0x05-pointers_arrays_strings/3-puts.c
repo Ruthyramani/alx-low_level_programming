c451bbde4fd93e48095839e445a4a4b7259c4321
Ruth Charles <cruth7186@gmail.com>
2022-12-19 16:19:14 -0800

    printed a string in new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- *  _puts - print a string
- *  @str: string to be printed
+ * _puts - print a string
+ * @str: string to be  printed
  */
 
 void _puts(char *str)
@@ -10,5 +10,6 @@ void _puts(char *str)
 	{
 		_putchar(*str++);
 	}
+
 	_putchar('\n');
 }
